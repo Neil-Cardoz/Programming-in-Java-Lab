c24d9779b3ff64f435fd177c4362f758673ad2b5
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-01-16 19:48:38 +0530

    Introducing a Calculator class in Java, containing an addition method, leveraging an external UserInput class to accept user inputs, processing the sum of two integers, and returning the result.
    This commit introduces a Calculator class with an addition method that uses the UserInput class to gather two integers, calculates their sum, and returns the result. The design allows for future extension to other operations.

---
+++ Calculator/Calculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Calculator {
+
+    int addition() {
+        int[] num;
+        UserInput in = new UserInput();
+		
+		num = in.userInput();
+		int sum = num[0] + num[1];
+		
+		return sum;
+		     
+        
+	}
+	
+	
+	
+	
+}
