91fc3b1c632e578cd5cf1543118be1755b59731d
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-01-18 15:16:38 +0530

    Updated the Main.java file to include an option for calculating mean and variance of an array along with basic operations and Fibonacci sequence.
    Added options for mean, variance, and Fibonacci in Main.java.

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,8 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Creating an object of UserInput to display the menu and handle operations
+        // Creating an object of UserInput to display the menu
         UserInput userInput = new UserInput();
+        
+        // Creating an object of Calculator to perform operations
+        Calculator calc = new Calculator();
         
         // Display the menu to the user
         userInput.displayMenu();
